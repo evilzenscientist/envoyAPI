e8bbbe4fc7b8c044edba20a8966e2ccde1d46386
evilzenscientist <ezs@evilzenscientist.com>
2022-06-20 12:52:53 -0700

    Update entrez-local.ps1
    Updated to get clean JWT

--- entrez-local.ps1
+++ entrez-local.ps1
@@ -1,51 +1,37 @@
-# sample PowerShell to get API data from local Enphase Envoy
 # set some urls
-$e_login_url = "https://entrez.enphaseenergy.com/login"
-$e_token_url = "https://entrez.enphaseenergy.com/entrez_tokens"
-$e_local_envoy = "https://localipaddress"
+$e_login_url = "https://enlighten.enphaseenergy.com/login/login.json"
+$e_token_url = "https://entrez.enphaseenergy.com/tokens"
+$e_local_envoy = "https://localip"
+
 
 # set some secrets/creds
-$e_username = "Enphase username/email address"
-$e_password = "Strong password"
+# usually we would read these out of the Key Vault
+$e_username = "email@foo.com"
+$e_password = "ComplexPassword123!"
+$e_serNum = "numericsitecode"
 
-$e_site = "Site Name"
-$e_serNum = "Envoy Serial Number"
-
-# set up login POST body
+# set up login and token POST body
 $login_body = @{
-        username=$e_username
-        password=$e_password
+        'user[email]'=$e_username
+        'user[password]'=$e_password
         }
 
-# set up token POST body
+$loginstatus = Invoke-webrequest -Method POST -Uri $e_login_url -body $login_body -ContentType 'application/x-www-form-urlencoded'
+
+$session_id=(convertfrom-json $loginstatus.content).session_id
+
 $token_body = @{
-    Site=$e_site
-    serialNum=$e_serNum
-        }
+    'session_id'=$session_id
+    'serial_num'=$e_serNum
+    'username'=$e_username
+}
+$j_token_body = convertto-json $token_body -depth 1
 
-$contentType = 'application/x-www-form-urlencoded' 
-# login to Entrez token service
-# posting login_body as a form
-# create sessionvariable
-$loginstatus = Invoke-webrequest -Method POST -Uri $e_login_url -body $login_body -ContentType $contentType -sessionvariable entrezsession
-# reuse sessionvariable
-# retrieve html page containing JWT access token
-$authstatus = Invoke-webrequest -Method POST -Uri $e_token_url -body $token_body -ContentType $contentType -websession $entrezsession
+$token_response = Invoke-webrequest -Method POST -Uri $e_token_url -body $j_token_body -ContentType 'application/json'
 
-# build a regex pattern for the token
-# this is the risky area and may change in the future
-# read the raw $authstatus if this starts failing
-$tk_regex_pattern = '(?<=rows="10" \>).+?(?=\<)' 
-
-# extract the JWT from html body
-$auth_token = [regex]::Matches($authstatus.content, $tk_regex_pattern).value
-
-# ideally you should be testing the token here
-# a great function here that lets you do that https://www.michev.info/Blog/Post/2140/decode-jwt-access-and-id-tokens-via-powershell
-
-$s_auth_token = convertto-securestring -string $auth_token -AsPlainText -force
-
+$token = $token_response.content
 # assuming we have a valid token, it's good for an hour
+
 # login to the Envoy
 # locally issued certificate, so we need -SkipCertificateCheck
 # using newer PowerShell so -Authentication Bearer with the -Token
